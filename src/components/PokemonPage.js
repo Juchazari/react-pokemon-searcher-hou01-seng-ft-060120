bbdb4da3640ec7563fbe49ddeb389706d8d953a1
Julio <julio.chazari@gmail.com>
2020-08-12 00:13:39 -0500

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,17 +4,64 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+const POKEMON_URL = "http://localhost:3000/pokemon";
+
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    search: "",
+  }
+
+  componentDidMount() {
+    fetch(POKEMON_URL)
+      .then(res => res.json())
+      .then(pokemon => this.setState({ pokemon }))
+  }
+
+  pokeSearch = (e) => {
+    this.setState({
+      search: e.target.value,
+    })
+  }
+
+  handlePokeForm = (e, formData) => {
+    e.preventDefault()
+    e.target.reset()
+
+    const poke = {
+      "name": formData.name,
+      "hp": formData.hp,
+      "sprites": {
+        "front": formData.frontUrl,
+        "back": formData.backUrl
+      }
+    }
+
+    fetch(POKEMON_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(poke)
+    }).then(res => res.json())
+      .then(poke => {
+        const pokemon = [ ...this.state.pokemon, poke ]
+        this.setState({ pokemon })
+      })
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handlePokeForm={this.handlePokeForm} />
         <br />
-        <Search />
+        <Search pokeSearch={this.pokeSearch} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} pokeSearch={this.state.search} />
       </Container>
     )
   }
